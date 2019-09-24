71b3713290cb11af50532be4162735c8fab473b1
Guido Perman <guidoperman@gmail.com>
2019-09-24 05:09:23 -0700

    Interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interceptors';
+
+  constructor(private usuarioService:UsuarioService){
+    this.usuarioService.obtenerUsuario().subscribe(res=>{
+      console.log(res);
+    }, (error)=>{
+      console.log("Error en app component")
+    })
+  }
 }
